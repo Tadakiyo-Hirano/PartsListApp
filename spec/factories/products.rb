b5109c880c50146e7a94920bc4c789ab80e752e9
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-04-09 16:42:18 +0900

    factrybot修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :product do
-    FactoryBot.build(:category)
-    FactoryBot.build(:brand)
-
     model { "spec sample product" }
-    category_id { 1 }
-    brand_id { 1 }
   end
 end
