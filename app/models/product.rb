da8cc92bffc17c9f13a22eff7d8589d09c02b8c8
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-05-20 17:26:25 +0900

    リレーション修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,22 @@
 class Product < ApplicationRecord
   has_one_attached :document
+  belongs_to :brand
+  belongs_to :category
 
   validates :model, presence: true, length: { maximum: 50 }
   validates :category_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :brand_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  validate :category_id_that_does_not_exist
-  validate :brand_id_that_dose_not_exist
+  # validate :category_id_that_does_not_exist
+  # validate :brand_id_that_dose_not_exist
 
   # 登録するcategory_idの値が、紐づくCategoryのidの値に存在しない場合は無効にする。
-  def category_id_that_does_not_exist
-    errors.add(:base, 'カテゴリー選択エラー') unless Category.find_by(id: category_id)
-  end
+  # def category_id_that_does_not_exist
+  #   errors.add(:base, 'カテゴリー選択エラー') unless Category.find_by(id: category_id)
+  # end
 
   # 登録するbrand_idの値が、紐づくBrandのidの値に存在しない場合は無効にする。
-  def brand_id_that_dose_not_exist
-    errors.add(:base, 'ブランド選択エラー') unless Brand.find_by(id: brand_id)
-  end
+  # def brand_id_that_dose_not_exist
+  #   errors.add(:base, 'ブランド選択エラー') unless Brand.find_by(id: brand_id)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -73,10 +73,12 @@ ActiveRecord::Schema.define(version: 2021_05_17_024228) do
 
   create_table "products", force: :cascade do |t|
     t.string "model", default: "", null: false
-    t.integer "category_id", default: 0, null: false
-    t.integer "brand_id", default: 0, null: false
+    t.bigint "category_id"
+    t.bigint "brand_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -98,4 +100,6 @@ ActiveRecord::Schema.define(version: 2021_05_17_024228) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
 end
