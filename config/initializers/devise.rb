16320b9aef8da38676f7b2c3aea65637144df95d
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-06-15 17:34:30 +0900

    circleci修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,8 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'akitamac@gmail.com'
-  # config.mailer_sender = ENV['LOGIN_MAIL']
+  config.mailer_sender = ENV['LOGIN_MAIL']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
