8f5bcdde8b29635797d6ee9bf4f31fe4b4a2b76f
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-07-01 16:16:38 +0900

    user index追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,24 @@
 class Admins::UsersController < ApplicationController
-
+  before_action :authenticate_admin!
+  before_action :set_user, only: %i(show edit)
+  
   def index
     @users = User.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def user_current_sign_in_at(user)
+    user.current_sign_in_at.strftime("%Y/%m/%d %H:%M") unless user.current_sign_in_at == nil
+  end
+
+  def  user_last_sign_in_at(user)
+    user.last_sign_in_at.strftime("%Y/%m/%d %H:%M") unless user.current_sign_in_at == nil
+  end
 end
