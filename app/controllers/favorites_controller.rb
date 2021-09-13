a770e6399ac509f6b6853a9c8209e53075cd8360
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-09-13 14:13:49 +0900

    ui修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user
   
   def create
     product = Product.find(params[:product_id])
     favorite = Favorite.create(user_id: current_user.id, product_id: product.id)
-    redirect_back(fallback_location: root_path, flash: { success: "登録"})
+    redirect_back(fallback_location: root_path, flash: { success: "#{product.model}をブックマーク登録しました。"})
   end
 
   def destroy
     product = Product.find(params[:product_id])
     favorite = Favorite.find_by(user_id: current_user.id, product_id: product.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path, flash: { danger: "#{product.model}のブックマークを解除しました。" })
   end
 end
