7b241e348c6a59357eb872bce32bf782b2662860
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-09-28 15:30:11 +0900

    adminlteレイアウト

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -2,18 +2,22 @@ module BrandsHelper
   def new_brand_btn
     unless params[:id]
       link_to new_admins_brand_path, remote: true do
-        button_tag '新規登録', class: "btn btn-sm btn-primary"
+        button_tag '新規登録', class: "btn btn-sm btn-original shadow"
       end
     else
-      button_tag '新規登録', class: "btn btn-sm btn-outline-secondary", disabled: true
+      button_tag '新規登録', class: "btn btn-sm btn-outline-secondary shadow", disabled: true
     end
   end
 
   def delete_brand_btn
     unless params[:id]
-      button_tag '削除', class: "btn btn-sm btn-danger", form: "delete-brand", url: destroy_all_admins_brands_path, method: :delete, data: { disable_with: '送信中', confirm: '選択したカテゴリーを削除しますか？' }
+      button_tag '削除', class: "btn btn-sm btn-danger shadow",
+                          form: "delete-brand",
+                          url: destroy_all_admins_brands_path,
+                          method: :delete,
+                          data: { disable_with: '送信中', confirm: "選択した#{Brand.human_attribute_name :name}を削除しますか？" }
     else
-      button_tag '削除', class: "btn btn-sm btn-outline-secondary", disabled: true
+      button_tag '削除', class: "btn btn-sm btn-outline-secondary shadow", disabled: true
     end
   end
 
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,18 +2,18 @@ module CategoriesHelper
   def new_category_btn
     unless params[:id]
       link_to new_admins_category_path, remote: true do
-        button_tag '新規登録', class: "btn btn-sm btn-primary"
+        button_tag '新規登録', class: "btn btn-sm btn-original shadow"
       end
     else
-      button_tag '新規登録', class: "btn btn-sm btn-outline-secondary", disabled: true
+      button_tag '新規登録', class: "btn btn-sm btn-outline-secondary shadow", disabled: true
     end
   end
 
   def delete_category_btn
     unless params[:id]
-      button_tag '削除', class: "btn btn-sm btn-danger", form: "delete-category", url: destroy_all_admins_categories_path, method: :delete, data: { disable_with: '送信中', confirm: '選択したカテゴリーを削除しますか？' }
+      button_tag '削除', class: "btn btn-sm btn-danger shadow", form: "delete-category", url: destroy_all_admins_categories_path, method: :delete, data: { disable_with: '送信中', confirm: '選択したカテゴリーを削除しますか？' }
     else
-      button_tag '削除', class: "btn btn-sm btn-outline-secondary", disabled: true
+      button_tag '削除', class: "btn btn-sm btn-outline-secondary shadow", disabled: true
     end
   end
 
