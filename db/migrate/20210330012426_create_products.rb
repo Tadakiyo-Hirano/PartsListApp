95d65702f46db429d8d8c94ad76879c01343b141
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-10-12 14:19:21 +0900

    モーダルボタン配置修正

--- db/migrate/20210330012426_create_products.rb
+++ db/migrate/20210330012426_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :model, default: "", null: false
+      t.integer :view_level, default: 1, null: false
       t.references :category, foreign_key: true
       t.references :brand, foreign_key: true
 
