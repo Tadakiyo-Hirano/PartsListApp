851ecd7b08986f9237c8fa18d17b86470eab0c35
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-10-27 16:50:07 +0900

    loginバリデーション追加

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Admins::SessionsController < Devise::SessionsController
+  before_action :user_loged_in?
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,11 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+    def user_loged_in?
+      redirect_to user_products_path(current_user) if user_signed_in?
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :admin_loged_in?
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,11 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+    def admin_loged_in?
+      redirect_to admins_products_path if admin_signed_in?
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
